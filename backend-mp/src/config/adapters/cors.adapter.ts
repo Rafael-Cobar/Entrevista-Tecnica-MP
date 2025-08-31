93bab2e434c58f28dc914401903c8bbe194ca04a
Rafael Cobar <rafaelcobarg@gmail.com>
2025-08-31 11:20:14 -0600

    feat: cambios en cors de api

--- backend-mp/src/config/adapters/cors.adapter.ts
+++ backend-mp/src/config/adapters/cors.adapter.ts
@@ -1,14 +1,17 @@
-import cors from "cors";
+import cors from 'cors';
 
 export const corsMiddleware = (acceptedOrigins: string) =>
 	cors({
 		origin: (origin, callback) => {
 			if (!origin) return callback(null, true);
 
-			const origins = acceptedOrigins.split(",");
+			const origins = acceptedOrigins.split(/[,;]/).map((o) => o.trim());
+
+			if (origins.includes('*')) return callback(null, true);
+
 			if (origins.includes(origin)) return callback(null, true);
 
-			return callback(new Error("Not allowed by CORS"));
+			return callback(new Error('Not allowed by CORS'));
 		},
-		credentials: true,
+		credentials: true
 	});
